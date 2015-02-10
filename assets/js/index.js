cadee5a1d2a9a6ea9124a58063943e0c8ea41a74
Max Fang <maxfangx@gmail.com>
2015-02-10 01:23:42 -0800

    Remove slow ass jQuery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,17 @@
-$('#menu-toggle').click(function(e) {
-    e.preventDefault();
-    $('#wrapper').toggleClass("toggled");
-});
+// $('#menu-toggle').click(function(e) {
+//     e.preventDefault();
+//     $('#wrapper').toggleClass("toggled");
+// });
+
+document.querySelector('#menu-toggle').onclick = function() {
+	if(document.querySelector('#wrapper').classList.contains('toggled')){
+		document.querySelector('#wrapper').classList.remove('toggled');
+	}
+	else {
+		document.querySelector('#wrapper').classList.add('toggled');
+	}
+}
+
+// document.onclick = function() {
+// 	window.alert("test 2");
+// }
