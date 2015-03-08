18cb8d07cd13de225514640524dc01c22896a5cb
Max Fang <maxfangx@gmail.com>
2015-03-07 19:41:57 -0800

    Correct database configuration

--- host/settings.py
+++ host/settings.py
@@ -60,8 +60,11 @@ WSGI_APPLICATION = 'host.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('EAGLE_DB_NAME'),
+        'USER': os.getenv('EAGLE_USER'),
+        'PASSWORD': os.getenv('EAGLE_PASS'),
+        'PORT': os.getenv('EAGLE_DB_PORT'),
     }
 }
 
