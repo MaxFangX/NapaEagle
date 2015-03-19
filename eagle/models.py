25f967a8725732deb2694674eb71efafd62a8bf2
Max Fang <maxfangx@gmail.com>
2015-03-18 21:53:12 -0700

    Wine Model

--- eagle/models.py
+++ eagle/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Wine(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.CharField(max_length=1000)
+    name = models.CharField(max_length=256,
+                                default='N/A')
+    location = models.CharField(max_length=256,
+                                default='N/A')
+    description = models.CharField(max_length=1000,
+                                default="N/A")
+    image_url = models.CharField(max_length=1000, 
+            default="http://i.imgur.com/CnLHeey.jpg") #Generic image
 
