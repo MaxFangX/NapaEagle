363f903aef499a83b6ca2f09796bb59bd4e7a833
Max Fang <maxfangx@gmail.com>
2015-03-19 23:53:01 -0700

    Views.py to load data

--- eagle/views.py
+++ eagle/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from eagle.models import Wine
 
 class HomeView(TemplateView):
     template_name = "eagle/index.html"
 
-class CatalogView(TemplateView):
+class CatalogView(ListView):
+    model = Wine
     template_name = "eagle/catalog.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(CatalogView, self).get_context_data(**kwargs)
