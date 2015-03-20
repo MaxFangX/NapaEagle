cef1b29d7757132214553331957a1469d591fe7c
Max Fang <maxfangx@gmail.com>
2015-03-20 00:44:07 -0700

    Price

--- eagle/models.py
+++ eagle/models.py
@@ -3,14 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Wine(models.Model):
-    name = models.CharField(max_length=256,
-                                default='N/A')
-    location = models.CharField(max_length=256,
-                                default='N/A')
-    description = models.CharField(max_length=1000,
-                                default="N/A")
+    name = models.CharField(max_length=256)
+    location = models.CharField(max_length=256)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     image_url = models.CharField(max_length=1000, 
-            default="http://i.imgur.com/CnLHeey.jpg") #Generic image
+                                default="http://i.imgur.com/CnLHeey.jpg") #Generic image
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     
