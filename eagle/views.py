d08528724d8d7ce71423437b214db14073aef888
Max Fang <maxfangx@gmail.com>
2015-03-26 21:22:30 -0700

    Finish removing namespace

--- eagle/views.py
+++ eagle/views.py
@@ -4,11 +4,11 @@ from django.views.generic import TemplateView, ListView
 from eagle.models import Wine
 
 class HomeView(TemplateView):
-    template_name = "eagle/index.html"
+    template_name = "index.html"
 
 class CatalogView(ListView):
     model = Wine
-    template_name = "eagle/catalog.html"
+    template_name = "catalog.html"
 
     def get_context_data(self, **kwargs):
         context = super(CatalogView, self).get_context_data(**kwargs)
