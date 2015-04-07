a34c4063e8ccea20e744efc843aeca94f4047b9d
Max Fang <maxfangx@gmail.com>
2015-04-06 22:39:51 -0700

    Navbar now becomes transparent

--- eagle/static/js/index.js
+++ eagle/static/js/index.js
@@ -1,5 +1,20 @@
-console.log($(window).height());
-
-$('.btn-primary').click(function() {
-    $('.navbar').css("background", "transparent"));
+/* Responsive navbar */
+var transparent = false;
+var locked = false;
+$(document).on('scroll', function(e) {
+    if (transparent && $(document).scrollTop() == 0 && !locked) {
+        locked = true;
+        $('.navbar').animate({
+            'opacity': 1
+        }, 500);
+        transparent = false;
+        locked = false;
+    } else if (!transparent && $(document).scrollTop() != 0 && !locked) {
+        locked = true;
+        $('.navbar').animate({
+            'opacity': 0
+        }, 500);
+        transparent = true;
+        locked = false;
+    };
 });
