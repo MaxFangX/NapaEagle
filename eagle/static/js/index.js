08e536d051fc71316ac5cbb2470480ea27464fcf
Max Fang <maxfangx@gmail.com>
2015-04-08 01:51:50 -0700

    Disable responsive navbar

--- eagle/static/js/index.js
+++ eagle/static/js/index.js
@@ -1,20 +1,29 @@
 /* Responsive navbar */
-var transparent = false;
+/*var transparent = false;
 var locked = false;
 $(document).on('scroll', function(e) {
-    if (transparent && $(document).scrollTop() == 0 && !locked) {
+    if (transparent && $(document).scrollTop() === 0 && !locked) {
         locked = true;
         $('.navbar').animate({
-            'opacity': 1
+            'opacity': 1,
+            'color': '#888'
+        }, 500);
+        $('.navbar-header').animate({
+            'color': '#FFF'
         }, 500);
         transparent = false;
         locked = false;
-    } else if (!transparent && $(document).scrollTop() != 0 && !locked) {
+    } else if (!transparent && $(document).scrollTop() !== 0 && !locked) {
         locked = true;
         $('.navbar').animate({
-            'opacity': 0
+            'opacity': 0.2,
+            'color': 'transparent'
+        }, 500);
+        $('.navbar-header').animate({
+            'color': '#FFF'
         }, 500);
         transparent = true;
         locked = false;
-    };
+    }
 });
+*/
