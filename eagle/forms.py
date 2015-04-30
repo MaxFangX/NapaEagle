55415df7919bb05c78c04d5f3a23caee60d0f63b
Max Fang <maxfangx@gmail.com>
2015-04-30 16:46:41 -0700

    Added form object

---
+++ eagle/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    your_name = forms.CharField(label="Your name", max_length=100)
+    your_email = forms.CharField(label="Your email", max_length=256)
+    message = forms.CharField(label="Message", max_length=5000)
