cbf7ed1a9529cd8a06772e3eafb0b78d912a2ae0
Max Fang <maxfangx@gmail.com>
2015-05-10 22:59:41 -0700

    Use text area

--- eagle/forms.py
+++ eagle/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ContactForm(forms.Form):
     your_name = forms.CharField(label="Your name", max_length=100)
     your_email = forms.CharField(label="Your email", max_length=256)
-    message = forms.CharField(label="Message", max_length=5000)
+    message = forms.CharField(label="Message", widget=forms.Textarea, max_length=5000)
