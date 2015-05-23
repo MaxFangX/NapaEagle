8b7d2cc70840a75c5b174e258c854e6c81550850
Max Fang <maxfangx@gmail.com>
2015-05-23 16:04:48 -0700

    Connection pooling

--- host/settings.py
+++ host/settings.py
@@ -92,5 +92,9 @@ STATIC_URL = '/static/'
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
+
+# Enable Connection Pooling
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
