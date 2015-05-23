cdb8a7fdadf7cc26b634ae4421d5bc9e3cbb3148
Max Fang <maxfangx@gmail.com>
2015-05-23 16:29:17 -0700

    Add static files dir

--- host/settings.py
+++ host/settings.py
@@ -88,6 +88,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
