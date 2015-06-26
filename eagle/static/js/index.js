4b6b018e013d313d3726e8bfb336d45818e3f7a9
Max Fang <maxfangx@gmail.com>
2015-06-26 14:05:18 -0700

    Add logo

--- eagle/static/js/index.js
+++ eagle/static/js/index.js
@@ -53,31 +53,33 @@ window.onresize = function(event) {
     updateSectionIndexes();
 };
 
+document.querySelector('#gallery-nav').addEventListener("click", function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
 
-var pswpElement = document.querySelectorAll('.pswp')[0];
+    // build items array
+    var items = [
+        {
+            src: 'http://i.imgur.com/7XJKPA7.jpg',
+            w: 600,
+            h: 400
+        },
+        {
+            src: 'http://upload.wikimedia.org/wikipedia/commons/2/23/Chinese_railroad_workers_sierra_nevada.jpg',
+            w: 1200,
+            h: 900
+        }
+    ];
 
-// build items array
-var items = [
-    {
-        src: 'http://i.imgur.com/7XJKPA7.jpg',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'http://upload.wikimedia.org/wikipedia/commons/2/23/Chinese_railroad_workers_sierra_nevada.jpg',
-        w: 1200,
-        h: 900
-    }
-];
+    // define options (if needed)
+    var options = {
+        // optionName: 'option value'
+        // for example:
+        index: 0 // start at first slide
+    };
 
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-};
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+});
 
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
 
