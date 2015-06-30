9fd5c01ec5497ddb4dd468c6113778a9188c6a3d
Max Fang <maxfangx@gmail.com>
2015-06-29 23:51:24 -0700

    Remove some stuff

--- eagle/urls.py
+++ eagle/urls.py
@@ -1,11 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from eagle import views
-#Use login_required later for private pages
-from django.contrib.auth.decorators import login_required
 
-urlpatterns = patterns('', 
+urlpatterns = patterns(
+    '',
     url(r'^$', views.homepage, name='homepage'),
-    url(r'^catalog/$', views.CatalogView.as_view(), name='catalog')    
+    url(r'^catalog/$', views.CatalogView.as_view(), name='catalog')
 )
-
-
