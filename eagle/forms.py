55c0e55c62456e6a9cf0bb3a00b499be7f8b268a
Max Fang <maxfangx@gmail.com>
2015-07-02 23:59:21 -0700

    Quick lint

--- eagle/forms.py
+++ eagle/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
     your_name = forms.CharField(label="Your name", max_length=100)
     your_email = forms.CharField(label="Your email", max_length=256)
