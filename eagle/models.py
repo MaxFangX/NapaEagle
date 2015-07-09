353a2679c26502d4f986c42799bb9ed4fc43d1a6
Max Fang <maxfangx@gmail.com>
2015-07-09 10:36:16 -0700

    Add Profile model

--- eagle/models.py
+++ eagle/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Wine(models.Model):
     name = models.CharField(max_length=256)
     location = models.CharField(max_length=256)
     description = models.CharField(max_length=1000)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    image_url = models.CharField(max_length=1000, 
-                                default="http://i.imgur.com/CnLHeey.jpg") #Generic image
+    image_url = models.CharField(max_length=1000, null=False)
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    referrer = models.ForeignKey(User, null=True, related_name='+')
+    full_name = models.CharField(null=True, blank=True, max_length=128)
